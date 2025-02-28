d82f0f32532daf4bbbb0c37df50191deddcbc7f4
Klnreddy03 <111518463+Klnreddy03@users.noreply.github.com>
2025-02-27 23:20:19 -0600

    Update App.js

--- travel-app/src/App.js
+++ travel-app/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'; // Make sure to import 'Navigate'
-import LoginPage from './LoginPage';
-import RegisterPage from './RegisterPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./LandingPage";
+import LoginPage from "./LoginPage";
+import RegisterPage from "./RegisterPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/" element={<Navigate to="/login" />} /> {/* Redirect to login by default */}
       </Routes>
     </Router>
   );
-};
+}
 
 export default App;
